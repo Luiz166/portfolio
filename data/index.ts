a3eb94fe00d4900964e2481b4ac8c80629ea95bf
Luiz <otavio.tata.morais@gmail.com>
2024-09-29 12:51:46 -0300

    adicionando arquivos

---
+++ data/index.ts
@@ -0,0 +1,160 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Priorizo a colaboração do cliente, promovendo a comunicação aberta",
+      description: "",
+      className: "lg:col-span-1 md:col-span-3 md:row-span-1",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+        id: 2,
+        title: "Estou aberto a propostas internacionais",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+    },
+    {
+      id: 3,
+      title: "Meu stack",
+      description: "Sempre procurando melhorar",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 5,
+      title: "Entusiasta da tecnologia com paixão por desenvolvimento",
+      description: "",
+      className: "lg:col-span-1 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 6,
+      title: "Quer começar um projeto comigo?",
+      description: "",
+      className: "lg:col-span-3 md:col-span-3 md:row-span-1 row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "To-do-list",
+      description: "Uma lista de afazeres usando react.js, node e mysql",
+      img: "/to-do-list.png",
+      iconLists: ["/re.svg", "/tail.svg", "/node.svg"],
+      link: "https://github.com/luiz166/to-do-list/",
+    },
+    {
+      id: 2,
+      title: 'Eventify',
+      description: "Aplicação de gestão de eventos comemorativos",
+      img: "",
+      iconLists: ["/re.svg", "/tail.svg", "/next.svg", "/psql.svg", "/prisma.svg"],
+      link: "https://github.com/luiz166/",
+    },
+  ];
+
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev - JSM Tech",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/luiz166"
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      link : "#"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/luiz-otávio-morais-916b141b8/"
+    },
+  ];
